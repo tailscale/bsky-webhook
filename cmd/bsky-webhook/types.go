8d43649a26f7ac57325a2c34494cdbb376c10cc3
M. J. Fromberger <michael.j.fromberger@gmail.com>
2024-11-14 02:55:02 -0800

    cmd: reorganize code a bit (#1)
    * cmd: reorganize code a bit
    
    As preparation to add secrets management and optional tailscale support,
    reorganize the code a bit. Specifically:
    
    - Add some doc comments where apropos.
    - Group the flag variables (cosmetic, for legibility).
    - Use conventional flag naming (snake vs. camel).
    - Add additional checks for required flag/env settings.
    - Move helper functions below main.
    - Clean up a few variable names per https://go.dev/wiki/CodeReviewComments#initialisms
    - Move the JSON type wrappers to a separate file.
    - Remove an unnecessary signal handler (the default already does this).
    - Pull out the round-robin address mapping into a function.
    - Move the stateless compressor to the top level.
    
    * fix doc comment
    
    * drop required marking for PDS
    
    * replace weird but apparently-necessary zstd option

---
+++ cmd/bsky-webhook/types.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"net/url"
+)
+
+type BskyMessage struct {
+	Did    string      `json:"did"`
+	Commit *BskyCommit `json:"commit"`
+	Kind   string      `json:"kind"`
+}
+
+func (m *BskyMessage) toURL(handle *string) string {
+	author := handle
+	if author == nil {
+		author = &m.Did
+	}
+
+	return fmt.Sprintf("https://bsky.app/profile/%s/post/%s", url.PathEscape(*author), url.PathEscape(m.Commit.Rkey))
+}
+
+type BskyCommit struct {
+	Rev       string      `json:"rev"`
+	Rkey      string      `json:"rkey"`
+	Record    *BskyRecord `json:"record"`
+	Operation string      `json:"operation"`
+}
+
+type BskyRecord struct {
+	Text  string    `json:"text"`
+	Embed BskyEmbed `json:"embed"`
+}
+
+type BskyEmbed struct {
+	Images []BskyImage `json:"images"`
+}
+
+type BskyImage struct {
+	Image BskyInnerImage `json:"image"`
+}
+
+type BskyInnerImage struct {
+	Ref BskyImageRef `json:"ref"`
+}
+
+type BskyImageRef struct {
+	Link string `json:"$link"`
+}
+
+type SlackAttachment struct {
+	AuthorName string `json:"author_name"`
+	AuthorIcon string `json:"author_icon"`
+	AuthorLink string `json:"author_link"`
+	Text       string `json:"text"`
+	ImageUrl   string `json:"image_url"`
+	Footer     string `json:"footer"`
+}
+
+type SlackBody struct {
+	Text        string            `json:"text"`
+	UnfurlLinks bool              `json:"unfurl_links"`
+	UnfurlMedia bool              `json:"unfurl_media"`
+	Attachments []SlackAttachment `json:"attachments"`
+}
